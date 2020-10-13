3721423992e371ce926db4b1b6fdbb048cc0ab5c
Connum <connum@gmail.com>
2020-10-13 22:50:13 +0200

    fix: constructor argument $shortcodes was never used

--- class-shortcode-tree.php
+++ class-shortcode-tree.php
@@ -13,7 +13,7 @@ class Shortcode {
 		$this->name = $name;
 		$this->atts = $atts;
 		$this->content = $content;
-		$this->shortcodes = array ();
+		$this->shortcodes = $shortcodes;
 		
 		$this->parent = $parent;
 	}
