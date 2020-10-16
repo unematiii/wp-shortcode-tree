5ad1c34b82a984c908145c688442cb9dfb382ea8
Mati Kärner <mati.karner@gmail.com>
2020-10-16 22:23:08 +0300

    WIP [chore] Introduce phpcs with WordPress coding standards (#9)
    [chore] Introduce phpcs with WordPress coding standards

--- src/class-shortcode-tree.php
+++ src/class-shortcode-tree.php
@@ -1,78 +1,198 @@
 <?php
+/**
+ * Implementation of ShortCode and ShortcodeTree classes
+ *
+ * @package WordPress
+ */
 
 namespace WordPress;
 
+/**
+ * Represents a single shortcode together with its attributes and siblings
+ */
 class Shortcode {
+	/**
+	 * Shortcode (tag) name
+	 *
+	 * @var string
+	 */
 	protected $name;
+
+	/**
+	 * Shortcode attributes (associative array of key/value pairs)
+	 *
+	 * @var array
+	 */
 	protected $atts;
+
+	/**
+	 * Shortcode content
+	 *
+	 * @var string
+	 */
 	protected $content;
+
+	/**
+	 * Parent node of this shortcode. Null, if shortcode has no parent
+	 *
+	 * @var \WordPress\ShortCode|NULL
+	 */
 	protected $parent;
-	protected $shortcodes = array ();
+
+	/**
+	 * Array of siblings as instances of \WordPress\ShortCode
+	 *
+	 * @var array
+	 */
+	protected $shortcodes = array();
+
+	/**
+	 * Determines if shortcode has a closing tag
+	 *
+	 * @var boolean
+	 */
 	protected $closed = false;
 
+	/**
+	 * Construct new ShortCode
+	 *
+	 * @param string                    $name Shortcode name.
+	 * @param array                     $atts Shortcode attributes (associative array of key/value pairs).
+	 * @param string                    $content Content of shortcode (text).
+	 * @param array                     $shortcodes Array of siblings as instances of \WordPress\ShortCode.
+	 * @param \WordPress\ShortCode|NULL $parent Parent node of this shortcode. Null, if shortcode has no parent.
+	 */
 	public function __construct($name = '', $atts = array(), $content = '', $shortcodes = array(), $parent = null) {
-		$this->name = $name;
-		$this->atts = $atts;
-		$this->content = $content;
+		$this->name       = $name;
+		$this->atts       = $atts;
+		$this->content    = $content;
 		$this->shortcodes = $shortcodes;
 
 		$this->parent = $parent;
 	}
 
+	/**
+	 * Gets shortcode name
+	 *
+	 * @return string
+	 */
 	public function getName() {
 		return $this->name;
 	}
 
+	/**
+	 * Sets shortcode name
+	 *
+	 * @param string $name Shortcode name.
+	 * @return void
+	 */
 	public function setName($name) {
 		$this->name = $name;
 	}
 
+	/**
+	 * Gets/sets attribute value:
+	 *  if $value is not provided, returns attributes value
+	 *  if $value is provided, sets attributes value
+	 *
+	 * @param string $name Attribute name.
+	 * @param string $value Attribute value.
+	 * @return void|string
+	 */
 	public function attr($name = null, $value = null) {
-		if (isset ( $this->atts [$name] )) {
-			if (is_null ( $value ))
-				return $this->atts [$name];
-			else
-				$this->atts [$name] = $value;
+		if (isset( $this->atts [ $name ] ) && is_null( $value )) {
+			return $this->atts [ $name ];
+		} else {
+			$this->atts [ $name ] = $value;
 		}
 	}
 
+	/**
+	 * Returns associative array of shortcode attributes as a reference
+	 *
+	 * @return array
+	 */
 	public function &atts() {
 		return $this->atts;
 	}
 
+	/**
+	 * Returns true if shortcode has a closing tag, false otherwise
+	 *
+	 * @return boolean
+	 */
 	public function getClosed() {
 		return $this->closed;
 	}
 
+	/**
+	 * Marks shortcode as closing or self-closing
+	 *
+	 * @param boolean $state If true, shortcode will be serialized with closing tag.
+	 * @return void
+	 */
 	public function setClosed($state = true) {
 		$this->closed = $state;
 	}
 
+	/**
+	 * Gets shortcode content
+	 *
+	 * @return string
+	 */
 	public function getContent() {
 		return $this->content;
 	}
 
+	/**
+	 * Sets shortcode content
+	 *
+	 * @param string $content Text content.
+	 * @return void
+	 */
 	public function setContent($content) {
 		$this->content = $content;
 	}
 
+	/**
+	 * Gets shortcode parent. Returns NULL if this short has no root node.
+	 *
+	 * @return \WordPress\ShortCode|NULL
+	 */
 	public function getParent() {
 		return $this->parent;
 	}
 
+	/**
+	 * Sets shortcode parent
+	 *
+	 * @param \WordPress\ShortCode $parent Parent node.
+	 * @return void
+	 */
 	public function setParent($parent) {
 		if ($parent instanceof Shortcode) {
 			$this->parent = $parent;
 		}
 	}
 
+	/**
+	 * Adds another shortcode as a sibling and marks itself as a root of that shortcode
+	 *
+	 * @param \WordPress\ShortCode $shortcode Shortcode.
+	 * @return void
+	 */
 	public function add_shortcode($shortcode) {
 		if ($shortcode instanceof Shortcode) {
-			$shortcode->setParent ( $this );
-			array_push ( $this->shortcodes, $shortcode );
+			$shortcode->setParent( $this );
+			array_push( $this->shortcodes, $shortcode );
 		}
 	}
 
+	/**
+	 * Returns array of shortcodes as a reference
+	 *
+	 * @return array
+	 */
 	public function &shortcodes() {
 		return $this->shortcodes;
 	}
@@ -80,85 +200,110 @@ class Shortcode {
 	/**
 	 * Find the n-th occurrence of the shortcode in the tree, where n (= $index) is zero-based
 	 *
-	 * @param string $shortcode_name
-	 * @param number $index
-	 * @param number $c
+	 * @param string $shortcode_name Shortcode name.
+	 * @param number $index n-th occurrence (zero-based).
+	 * @param number $c Optional. Counter for determining occurrences found so far.
 	 * @return \WordPress\ShortCode|NULL
 	 */
 	public function findNthOccurrence($shortcode_name, $index, &$c = 0) {
 		$index = (int) $index;
 
-		if ($shortcode_name === $this->name)
-			if ($c === $index)
+		if ($shortcode_name === $this->name) {
+			if ($c === $index) {
 				return $this;
-			else
+			} else {
 				++ $c;
+			}
+		}
 
-		if (! count ( $this->shortcodes ))
+		if ( ! count( $this->shortcodes )) {
 			return null;
+		}
 
-		$occurrences = array ();
-		foreach ( $this->shortcodes as $shortcode )
-			array_push ( $occurrences, $shortcode->findNthOccurrence ( $shortcode_name, $index, $c ) );
+		$occurrences = array();
+		foreach ( $this->shortcodes as $shortcode ) {
+			array_push( $occurrences, $shortcode->findNthOccurrence( $shortcode_name, $index, $c ) );
+		}
 
-		$occurrences = array_filter ( $occurrences, function ($o) {
-			return ! is_null ( $o );
-		} );
+		$occurrences = array_filter(
+			$occurrences,
+			function ($o) {
+				return ! is_null( $o );
+			}
+		);
+		$occurrence  = reset( $occurrences );
 
-		return reset( $occurrences ) ?: null;
+		return $occurrence ? $occurrence : null;
 	}
 
+	/**
+	 * Traverses child nodes recursively and finds all shortcodes that match specified name
+	 *
+	 * @param string $shortcode_name Name to look up shortcodes by.
+	 * @return array
+	 */
 	public function findAll ($shortcode_name) {
 		$result = array();
-		if ($shortcode_name === $this->name)
-			array_push($result, $this);
+		if ($shortcode_name === $this->name) {
+			array_push( $result, $this );
+		}
 
-		if (count ( $this->shortcodes ))
+		if (count( $this->shortcodes )) {
 			foreach ($this->shortcodes as $shortcode) {
-				$result = array_merge($result, $shortcode->findAll($shortcode_name));
+				$result = array_merge( $result, $shortcode->findAll( $shortcode_name ) );
 			}
+		}
 
 		return $result;
 	}
 
+	/**
+	 * Parses string of shortcodes into array of \Wordpress\ShortCode.
+	 * For single rooted hierarchy, array only contains one root node.
+	 *
+	 * @param string $shortcode Input string to parse.
+	 * @return array
+	 */
 	public static function fromString($shortcode) {
-		$pattern = get_shortcode_regex ();
-		$nodes = array ();
+		$pattern = get_shortcode_regex();
+		$nodes   = array();
 
-		if (preg_match_all ( "/$pattern/s", $shortcode, $matches )) {
-			$node_count = count ( $matches [0] );
+		if (preg_match_all( "/$pattern/s", $shortcode, $matches )) {
+			$node_count = count( $matches [0] );
 
-			for($node_index = 0; $node_index < $node_count; $node_index ++) {
-				if (strlen ( $matches [2] [$node_index] )) {
-					// Name
-					$name = $matches [2] [$node_index];
+			for ($node_index = 0; $node_index < $node_count; $node_index ++) {
+				if (strlen( $matches [2] [ $node_index ] )) {
+					/* Name */
+					$name = $matches [2] [ $node_index ];
 
-					// Attributes
-					$atts = shortcode_parse_atts ( $matches [3] [$node_index] );
+					/* Attributes */
+					$atts = shortcode_parse_atts( $matches [3] [ $node_index ] );
 
-					// Instantiate
-					$node = new Shortcode ( $name, '' === $atts ? array () : $atts );
+					/* Instantiate */
+					$node = new Shortcode( $name, '' === $atts ? array() : $atts );
 
-					// Siblings
-					$siblings = array ();
-					if (strlen ( $matches [5] [$node_index] )) {
-						if(0 === strpos( trim( $matches [5] [$node_index] ), '[' ))
-							$siblings = Shortcode::fromString ( $matches [5] [$node_index] );
-						else
-							$node->setContent($matches [5] [$node_index]);
-					}
-
-					foreach ( $siblings as $sibling )
-						$node->add_shortcode ( $sibling );
-
-					if (0 === count($node->shortcodes)) {
-						if (preg_match("~\[/" . $name . "\]\s*$~", trim( $matches[0][$node_index] ))) {
-							$node->setClosed(true);
+					/* Siblings */
+					$siblings = array();
+					if (strlen( $matches [5] [ $node_index ] )) {
+						if (0 === strpos( trim( $matches [5] [ $node_index ] ), '[' )) {
+							$siblings = self::fromString( $matches [5] [ $node_index ] );
+						} else {
+							$node->setContent( $matches [5] [ $node_index ] );
 						}
 					}
 
-					// Processed
-					array_push ( $nodes, $node );
+					foreach ( $siblings as $sibling ) {
+						$node->add_shortcode( $sibling );
+					}
+
+					if (0 === count( $node->shortcodes )) {
+						if (preg_match( '~\[/' . $name . '\]\s*$~', trim( $matches[0][ $node_index ] ) )) {
+							$node->setClosed( true );
+						}
+					}
+
+					/* Processed */
+					array_push( $nodes, $node );
 				}
 			}
 		}
@@ -166,36 +311,56 @@ class Shortcode {
 		return $nodes;
 	}
 
+	/**
+	 * Serializes shortcode into string
+	 *
+	 * @return string
+	 */
 	public function __toString() {
-		$is_tag = ( bool ) strlen( trim ( $this->name ) );
-		$is_nested = ( bool ) count ( $this->shortcodes );
+		$is_tag            = (bool) strlen( trim( $this->name ) );
+		$is_nested         = (bool) count( $this->shortcodes );
 		$needs_closing_tag = $this->getClosed();
-		$has_content = ( bool ) strlen ( $this->content );
+		$has_content       = (bool) strlen( $this->content );
 
-		$out = '';
+		$out  = '';
 		$atts = $this->atts;
 
-		if ($is_tag)
+		if ($is_tag) {
 			$out .= '[' . $this->name;
+		}
 
-		if (count ( $this->atts ))
-			$out .= ' ' . implode ( ' ', array_map ( function ($key) use($atts) {
-				$value = $atts[$key];
-				if (is_int( $key )) {
-					return $value;
-				}
+		if (count( $this->atts )) {
+			$out .= ' ' . implode(
+				' ',
+				array_map(
+					function ($key) use($atts) {
+						$value = $atts[ $key ];
+						if (is_int( $key )) {
+							return $value;
+						}
 
-				return $key . '="' . $atts [$key] . '"';
-			}, array_keys ( $this->atts ) ) );
+						return $key . '="' . $atts [ $key ] . '"';
+					},
+					array_keys( $this->atts )
+				)
+			);
+		}
 
-		if ($is_tag)
+		if ($is_tag) {
 			$out .= ']';
+		}
 
 		if ($is_nested || $has_content) {
 			if ($is_nested) {
-				$out .= implode ( '', array_map ( function ($s) {
-					return ( string ) $s;
-				}, $this->shortcodes ) );
+				$out .= implode(
+					'',
+					array_map(
+						function ($s) {
+							return (string) $s;
+						},
+						$this->shortcodes
+					)
+				);
 			} else {
 				$out .= $this->content;
 			}
@@ -213,47 +378,81 @@ class Shortcode {
 	}
 }
 
+/**
+ * Represents a single rooted shortcode tree
+ */
 class ShortcodeTree {
+	/**
+	 * Root node of the tree
+	 *
+	 * @var \WordPress\ShortCode|NULL
+	 */
 	protected $root;
 
+	/**
+	 * Constructs new \WordPress\ShortcodeTree
+	 *
+	 * @param \WordPress\ShortCode|NULL $root Root node.
+	 */
 	public function __construct($root = null) {
-		if (! is_null ( $root ))
-			$this->setRoot ( $root );
+		if ( ! is_null( $root )) {
+			$this->setRoot( $root );
+		}
 	}
 
+	/**
+	 * Gets the root node of the tree
+	 *
+	 * @return \WordPress\ShortCode|NULL
+	 */
 	public function getRoot() {
 		return $this->root;
 	}
 
+	/**
+	 * Sets the root node of the tree
+	 *
+	 * @param \WordPress\ShortCode $root Root node.
+	 * @return void
+	 */
 	public function setRoot($root) {
-		if ($root instanceof Shortcode)
+		if ($root instanceof Shortcode) {
 			$this->root = $root;
+		}
 	}
 
 	/**
+	 * Constructs ShortCodeTree from string.
+	 * For non single rooted hierarchy, a dummy parent node is constructed.
 	 *
-	 * @param string $shortcode
+	 * @param string $shortcode Shortcode(s) to parse.
 	 * @return \WordPress\ShortCodeTree
 	 */
 	public static function fromString($shortcode) {
-		$tree = new ShortcodeTree ();
-		$nodes = Shortcode::fromString ( $shortcode );
+		$tree  = new ShortcodeTree();
+		$nodes = Shortcode::fromString( $shortcode );
 
-		if (count ( $nodes )) {
-			if (count ( $nodes ) > 1) {
-				$root = new Shortcode ();
-				foreach ( $nodes as $node )
-					$root->add_shortcode ( $node );
+		if (count( $nodes )) {
+			if (count( $nodes ) > 1) {
+				$root = new Shortcode();
+				foreach ( $nodes as $node ) {
+					$root->add_shortcode( $node );
+				}
 
-				$tree->setRoot ( $root );
+				$tree->setRoot( $root );
 			} else {
-				$tree->setRoot ( $nodes[0] );
+				$tree->setRoot( $nodes[0] );
 			}
 		}
 
 		return $tree;
 	}
 
+	/**
+	 * Serializes shortcode tree into string
+	 *
+	 * @return string
+	 */
 	public function __toString() {
 		return (string) $this->root;
 	}
--- tests/ShortcodeTreeTest.php
+++ tests/ShortcodeTreeTest.php
@@ -1,19 +1,18 @@
 <?php
 use \Wordpress\ShortcodeTree;
 
-class ShortcodeTreeTest extends \PHPUnit\Framework\TestCase
-{
-    public function setUp(): void {
-        // make a [test] shortcode available in all tests
-        add_shortcode('test', null);
-    }
+class ShortcodeTreeTest extends \PHPUnit\Framework\TestCase {
 
-    public function test_readmeExampleShouldWork()
-    {
-        add_shortcode('folder', null);
-        add_shortcode('document', null);
+	public function setUp(): void {
+		// Make a [test] shortcode available in all tests.
+		add_shortcode( 'test', null );
+	}
 
-        $post_content = '
+	public function test_readmeExampleShouldWork() {
+		add_shortcode( 'folder', null );
+		add_shortcode( 'document', null );
+
+		$post_content = '
         [folder name="SampleFolder"]
             [document type="image/png" size="64000" path="/path/to/image.png" name="File 1"]
             [document type="image/png" size="64000" path="/path/to/image.png" name="File 2"]
@@ -24,55 +23,55 @@ class ShortcodeTreeTest extends \PHPUnit\Framework\TestCase
         [/folder]
         ';
 
-        $expected_content = '[folder name="SampleFolder"][document type="image/png" size="64000" path="/path/to/image.png" name="My File 1"][document type="image/png" size="64000" path="/path/to/image.png" name="My File 2"][folder name="SampleFolder2"][document type="image/png" size="64000" path="/path/to/image.png" name="My File 3"][document type="image/png" size="64000" path="/path/to/image.png" name="My File 4"][/folder]';
+		$expected_content = '[folder name="SampleFolder"][document type="image/png" size="64000" path="/path/to/image.png" name="My File 1"][document type="image/png" size="64000" path="/path/to/image.png" name="My File 2"][folder name="SampleFolder2"][document type="image/png" size="64000" path="/path/to/image.png" name="My File 3"][document type="image/png" size="64000" path="/path/to/image.png" name="My File 4"][/folder]';
 
-        $content = ShortcodeTree::fromString ( $post_content );
-        $folder = $content->getRoot ();
+		$content = ShortcodeTree::fromString( $post_content );
+		$folder  = $content->getRoot();
 
-        $documents = $folder->findAll('document');
+		$documents = $folder->findAll( 'document' );
 
-        // Prepend 'My ' to filename
-        foreach($documents as $doc) {
-            $doc->attr('name', 'My ' . $doc->attr('name'));
-        }
+		// Prepend 'My ' to filename.
+		foreach ($documents as $doc) {
+			$doc->attr( 'name', 'My ' . $doc->attr( 'name' ) );
+		}
 
-        $this->assertEquals($expected_content, $content);
-    }
+		$this->assertEquals( $expected_content, $content );
+	}
 
-    public function test_immediatelyClosedEmptyTagsShouldWork() {
-        add_shortcode('av_tab_section', null);
-        add_shortcode('av_tab_sub_section', null);
-        add_shortcode('custom_sc', null);
+	public function test_immediatelyClosedEmptyTagsShouldWork() {
+		add_shortcode( 'av_tab_section', null );
+		add_shortcode( 'av_tab_sub_section', null );
+		add_shortcode( 'custom_sc', null );
 
-        $post_content = "[av_tab_section][av_tab_sub_section][custom_sc foo='bar'][/custom_sc][custom_sc][/custom_sc][/av_tab_sub_section][av_tab_section]";
-        $expected_content = '[av_tab_section][av_tab_sub_section][custom_sc foo="bar"][/custom_sc][custom_sc][/custom_sc][/av_tab_sub_section][av_tab_section]';
+		$post_content     = "[av_tab_section][av_tab_sub_section][custom_sc foo='bar'][/custom_sc][custom_sc][/custom_sc][/av_tab_sub_section][av_tab_section]";
+		$expected_content = '[av_tab_section][av_tab_sub_section][custom_sc foo="bar"][/custom_sc][custom_sc][/custom_sc][/av_tab_sub_section][av_tab_section]';
 
-        $content = ShortcodeTree::fromString ( $post_content );
+		$content = ShortcodeTree::fromString( $post_content );
 
-        $this->assertEquals($expected_content, (string)$content);
-    }
+		$this->assertEquals( $expected_content, (string) $content );
+	}
 
-    public function test_keyOnlyAttributesShouldWork() {
-        add_shortcode('av_one_third', null);
+	public function test_keyOnlyAttributesShouldWork() {
+		add_shortcode( 'av_one_third', null );
 
-        $post_content = "[av_one_third first min_height=''][test]";
-        $expected_content = '[av_one_third first min_height=""][test]';
+		$post_content     = "[av_one_third first min_height=''][test]";
+		$expected_content = '[av_one_third first min_height=""][test]';
 
-        $content = ShortcodeTree::fromString ( $post_content );
+		$content = ShortcodeTree::fromString( $post_content );
 
-        $this->assertEquals($expected_content, (string)$content);
-    }
+		$this->assertEquals( $expected_content, (string) $content );
+	}
 
-    public function test_constructorShouldBeAbleToSetRoot() {
-        add_shortcode('outer', null);
+	public function test_constructorShouldBeAbleToSetRoot() {
+		add_shortcode( 'outer', null );
 
-        $post_content = "[test][outer][inner][/outer][/test]";
-        $content = ShortcodeTree::fromString ( $post_content );
-        $outer = $content->getRoot()->findAll('outer');
-        $this->assertCount(1, $outer);
+		$post_content = '[test][outer][inner][/outer][/test]';
+		$content      = ShortcodeTree::fromString( $post_content );
+		$outer        = $content->getRoot()->findAll( 'outer' );
+		$this->assertCount( 1, $outer );
 
-        $outerNode = $outer[0];
-        $content = new ShortcodeTree($outerNode);
-        $this->assertEquals($outerNode, $content->getRoot());
-    }
+		$outerNode = $outer[0];
+		$content   = new ShortcodeTree( $outerNode );
+		$this->assertEquals( $outerNode, $content->getRoot() );
+	}
 }
