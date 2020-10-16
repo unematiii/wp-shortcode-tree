d6d490676e162da126f87429a66300fce6177998
Mati Kärner <mati.karner@gmail.com>
2020-10-16 09:35:08 +0300

    [chore] Introduce phpcs with WordPress coding standards

--- src/class-shortcode-tree.php
+++ src/class-shortcode-tree.php
@@ -7,13 +7,13 @@ class Shortcode {
 	protected $atts;
 	protected $content;
 	protected $parent;
-	protected $shortcodes = array ();
-	protected $closed = false;
+	protected $shortcodes = array();
+	protected $closed     = false;
 
 	public function __construct($name = '', $atts = array(), $content = '', $shortcodes = array(), $parent = null) {
-		$this->name = $name;
-		$this->atts = $atts;
-		$this->content = $content;
+		$this->name       = $name;
+		$this->atts       = $atts;
+		$this->content    = $content;
 		$this->shortcodes = $shortcodes;
 
 		$this->parent = $parent;
@@ -28,11 +28,12 @@ class Shortcode {
 	}
 
 	public function attr($name = null, $value = null) {
-		if (isset ( $this->atts [$name] )) {
-			if (is_null ( $value ))
-				return $this->atts [$name];
-			else
-				$this->atts [$name] = $value;
+		if (isset( $this->atts [ $name ] )) {
+			if (is_null( $value )) {
+				return $this->atts [ $name ];
+			} else {
+				$this->atts [ $name ] = $value;
+			}
 		}
 	}
 
@@ -68,8 +69,8 @@ class Shortcode {
 
 	public function add_shortcode($shortcode) {
 		if ($shortcode instanceof Shortcode) {
-			$shortcode->setParent ( $this );
-			array_push ( $this->shortcodes, $shortcode );
+			$shortcode->setParent( $this );
+			array_push( $this->shortcodes, $shortcode );
 		}
 	}
 
@@ -80,85 +81,96 @@ class Shortcode {
 	/**
 	 * Find the n-th occurrence of the shortcode in the tree, where n (= $index) is zero-based
 	 *
-	 * @param string $shortcode_name
-	 * @param number $index
-	 * @param number $c
+	 * @param string $shortcode_name Shortcode name.
+	 * @param number $index n-th occurrence (zero-based).
+	 * @param number $c Counter for determining occurrences found so far.
 	 * @return \WordPress\ShortCode|NULL
 	 */
 	public function findNthOccurrence($shortcode_name, $index, &$c = 0) {
 		$index = (int) $index;
 
-		if ($shortcode_name === $this->name)
-			if ($c === $index)
+		if ($shortcode_name === $this->name) {
+			if ($c === $index) {
 				return $this;
-			else
+			} else {
 				++ $c;
+			}
+		}
 
-		if (! count ( $this->shortcodes ))
+		if ( ! count( $this->shortcodes )) {
 			return null;
+		}
 
-		$occurrences = array ();
-		foreach ( $this->shortcodes as $shortcode )
-			array_push ( $occurrences, $shortcode->findNthOccurrence ( $shortcode_name, $index, $c ) );
+		$occurrences = array();
+		foreach ( $this->shortcodes as $shortcode ) {
+			array_push( $occurrences, $shortcode->findNthOccurrence( $shortcode_name, $index, $c ) );
+		}
 
-		$occurrences = array_filter ( $occurrences, function ($o) {
-			return ! is_null ( $o );
-		} );
+		$occurrences = array_filter(
+			$occurrences,
+			function ($o) {
+				return ! is_null( $o );
+			}
+		);
 
 		return reset( $occurrences ) ?: null;
 	}
 
 	public function findAll ($shortcode_name) {
 		$result = array();
-		if ($shortcode_name === $this->name)
-			array_push($result, $this);
+		if ($shortcode_name === $this->name) {
+			array_push( $result, $this );
+		}
 
-		if (count ( $this->shortcodes ))
+		if (count( $this->shortcodes )) {
 			foreach ($this->shortcodes as $shortcode) {
-				$result = array_merge($result, $shortcode->findAll($shortcode_name));
+				$result = array_merge( $result, $shortcode->findAll( $shortcode_name ) );
 			}
+		}
 
 		return $result;
 	}
 
 	public static function fromString($shortcode) {
-		$pattern = get_shortcode_regex ();
-		$nodes = array ();
+		$pattern = get_shortcode_regex();
+		$nodes   = array();
 
-		if (preg_match_all ( "/$pattern/s", $shortcode, $matches )) {
-			$node_count = count ( $matches [0] );
+		if (preg_match_all( "/$pattern/s", $shortcode, $matches )) {
+			$node_count = count( $matches [0] );
 
-			for($node_index = 0; $node_index < $node_count; $node_index ++) {
-				if (strlen ( $matches [2] [$node_index] )) {
-					// Name
-					$name = $matches [2] [$node_index];
+			for ($node_index = 0; $node_index < $node_count; $node_index ++) {
+				if (strlen( $matches [2] [ $node_index ] )) {
+					/* Name */
+					$name = $matches [2] [ $node_index ];
 
-					// Attributes
-					$atts = shortcode_parse_atts ( $matches [3] [$node_index] );
+					/* Attributes */
+					$atts = shortcode_parse_atts( $matches [3] [ $node_index ] );
 
-					// Instantiate
-					$node = new Shortcode ( $name, '' === $atts ? array () : $atts );
+					/* Instantiate */
+					$node = new Shortcode( $name, '' === $atts ? array() : $atts );
 
-					// Siblings
-					$siblings = array ();
-					if (strlen ( $matches [5] [$node_index] )) {
-						if(0 === strpos( trim( $matches [5] [$node_index] ), '[' ))
-							$siblings = Shortcode::fromString ( $matches [5] [$node_index] );
-						else
-							$node->setContent($matches [5] [$node_index]);
-					}
-
-					foreach ( $siblings as $sibling )
-						$node->add_shortcode ( $sibling );
-
-					if (0 === count($node->shortcodes)) {
-						if (preg_match("~\[/" . $name . "\]\s*$~", trim( $matches[0][$node_index] ))) {
-							$node->setClosed(true);
+					/* Siblings */
+					$siblings = array();
+					if (strlen( $matches [5] [ $node_index ] )) {
+						if (0 === strpos( trim( $matches [5] [ $node_index ] ), '[' )) {
+							$siblings = self::fromString( $matches [5] [ $node_index ] );
+						} else {
+							$node->setContent( $matches [5] [ $node_index ] );
 						}
 					}
 
-					// Processed
-					array_push ( $nodes, $node );
+					foreach ( $siblings as $sibling ) {
+						$node->add_shortcode( $sibling );
+					}
+
+					if (0 === count( $node->shortcodes )) {
+						if (preg_match( '~\[/' . $name . '\]\s*$~', trim( $matches[0][ $node_index ] ) )) {
+							$node->setClosed( true );
+						}
+					}
+
+					/* Processed */
+					array_push( $nodes, $node );
 				}
 			}
 		}
@@ -167,35 +179,50 @@ class Shortcode {
 	}
 
 	public function __toString() {
-		$is_tag = ( bool ) strlen( trim ( $this->name ) );
-		$is_nested = ( bool ) count ( $this->shortcodes );
+		$is_tag            = (bool) strlen( trim( $this->name ) );
+		$is_nested         = (bool) count( $this->shortcodes );
 		$needs_closing_tag = $this->getClosed();
-		$has_content = ( bool ) strlen ( $this->content );
+		$has_content       = (bool) strlen( $this->content );
 
-		$out = '';
+		$out  = '';
 		$atts = $this->atts;
 
-		if ($is_tag)
+		if ($is_tag) {
 			$out .= '[' . $this->name;
+		}
 
-		if (count ( $this->atts ))
-			$out .= ' ' . implode ( ' ', array_map ( function ($key) use($atts) {
-				$value = $atts[$key];
-				if (is_int( $key )) {
-					return $value;
-				}
+		if (count( $this->atts )) {
+			$out .= ' ' . implode(
+				' ',
+				array_map(
+					function ($key) use($atts) {
+						$value = $atts[ $key ];
+						if (is_int( $key )) {
+							return $value;
+						}
 
-				return $key . '="' . $atts [$key] . '"';
-			}, array_keys ( $this->atts ) ) );
+						return $key . '="' . $atts [ $key ] . '"';
+					},
+					array_keys( $this->atts )
+				)
+			);
+		}
 
-		if ($is_tag)
+		if ($is_tag) {
 			$out .= ']';
+		}
 
 		if ($is_nested || $has_content) {
 			if ($is_nested) {
-				$out .= implode ( '', array_map ( function ($s) {
-					return ( string ) $s;
-				}, $this->shortcodes ) );
+				$out .= implode(
+					'',
+					array_map(
+						function ($s) {
+							return (string) $s;
+						},
+						$this->shortcodes
+					)
+				);
 			} else {
 				$out .= $this->content;
 			}
@@ -217,8 +244,9 @@ class ShortcodeTree {
 	protected $root;
 
 	public function __construct($root = null) {
-		if (! is_null ( $root ))
-			$this->setRoot ( $root );
+		if ( ! is_null( $root )) {
+			$this->setRoot( $root );
+		}
 	}
 
 	public function getRoot() {
@@ -226,28 +254,31 @@ class ShortcodeTree {
 	}
 
 	public function setRoot($root) {
-		if ($root instanceof Shortcode)
+		if ($root instanceof Shortcode) {
 			$this->root = $root;
+		}
 	}
 
 	/**
+	 * Constructs ShortCodeTree from string
 	 *
-	 * @param string $shortcode
+	 * @param string $shortcode Shortcode(s) to parse.
 	 * @return \WordPress\ShortCodeTree
 	 */
 	public static function fromString($shortcode) {
-		$tree = new ShortcodeTree ();
-		$nodes = Shortcode::fromString ( $shortcode );
+		$tree  = new ShortcodeTree();
+		$nodes = Shortcode::fromString( $shortcode );
 
-		if (count ( $nodes )) {
-			if (count ( $nodes ) > 1) {
-				$root = new Shortcode ();
-				foreach ( $nodes as $node )
-					$root->add_shortcode ( $node );
+		if (count( $nodes )) {
+			if (count( $nodes ) > 1) {
+				$root = new Shortcode();
+				foreach ( $nodes as $node ) {
+					$root->add_shortcode( $node );
+				}
 
-				$tree->setRoot ( $root );
+				$tree->setRoot( $root );
 			} else {
-				$tree->setRoot ( $nodes[0] );
+				$tree->setRoot( $nodes[0] );
 			}
 		}
 
